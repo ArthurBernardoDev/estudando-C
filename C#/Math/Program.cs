455bb497bf19418a1d3cc555570748a5d03f600e
Arthur Bernardo <58444730+ArthurBernardoDev@users.noreply.github.com>
2021-05-09 14:38:03 -0300

    Math

---
+++ C#/Math/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // Math.Max(x,y) usado apra encontrar o maior valor de x e your
+         Console.WriteLine(Math.Max(5, 10);
+
+         // Math.Min(x,y) usado para encontrar o menor valor de x e y
+        Console.WriteLine(Math.Min(5,10));
+        
+        // Math.Sqrt(x) Raiz quadrada de x
+        Math.Sqrt(64);
+
+        // Math.Round() arrenda um número para o Número inteiro mais próximo
+        Math.Round(9.99);
+        }
+    }
+}
