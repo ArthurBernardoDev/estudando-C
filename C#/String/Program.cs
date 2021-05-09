9ff2342d2e416cfe03b51404165836d121083a4e
Arthur Bernardo <58444730+ArthurBernardoDev@users.noreply.github.com>
2021-05-09 17:14:08 -0300

    string part1

---
+++ C#/String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // strings são usadas para armazenar texto.
+
+         string greeting = "Hello";
+         Console.WriteLine(greeting);
+
+         string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+         Console.WriteLine("The length of the txt sting is: " + txt.Length);
+        
+        string txt1 = "Hello World";
+        Console.WriteLine(txt1.ToUpper()); // maisculo
+        Console.WriteLine(txt1.ToLower()); // minusculo
+        
+        // concatenação de strings
+        string firstName = "John ";
+        string lastName = "Doe";
+        string name = firstName + lastName;
+        Console.WriteLine(name);
+
+
+        string firstname = "Arthur";
+        string lastname = "bernardo";
+        string name1 = $"my full name é : {firstname} {lastname}";
+        Console.WriteLine(name1);
+        
+        }
+    }
+}
