0e45d410f5ef6e3412a61cb09250a5e91b948108
Arthur Bernardo <58444730+ArthurBernardoDev@users.noreply.github.com>
2021-05-09 22:38:26 -0300

    Booleans

---
+++ C#/Booleans/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Booleans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Para isso, C# tem um tipo de dados, que pode pegar os valores ou .booltruefalse
+            bool temCasa = true;
+            bool naoTemCasa = false;
+            Console.WriteLine(temCasa);
+            Console.WriteLine(naoTemCasa);
+
+            // expressão booleana é uma expressão que devolve um valor booleano ou true false
+            // exemplo 
+            int x = 10;
+            int y = 9;
+            Console.WriteLine(x > y);
+            //ou ate mais facil 
+            Console.WriteLine(10 > 9);
+
+            int x1 = 10;
+            Console.WriteLine(x1 == 10);
+
+            Console.WriteLine(10 == 15);
+        }
+    }
+}
