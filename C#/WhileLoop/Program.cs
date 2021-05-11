b58e1b57e0e7622c417c7d23ad822f78d18c3d7c
Arthur Bernardo <58444730+ArthurBernardoDev@users.noreply.github.com>
2021-05-11 17:07:42 -0300

    WhileLoop

---
+++ C#/WhileLoop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Os loops podem executar um bloco de código, desde que uma condição especificada seja alcançada.
+            // Os loops são úteis porque economizam tempo, reduzem os erros e tornam o código mais legível.
+        
+            // while (condição)
+            // {
+            //     bloco de codigo 
+            // }
+        
+
+            int i = 0;
+            while (i < 5){
+                Console.WriteLine(i);
+                i++;
+
+            }
+
+                // The Do/While Loop
+                // do
+                // {
+                //     bloco de codigo
+                // }
+                // while(condição)
+            int i1 = 0;
+            do {
+                Console.WriteLine(i1);
+                i++;
+            }
+            while (i1 < 5);
+
+        }
+    }
+}
