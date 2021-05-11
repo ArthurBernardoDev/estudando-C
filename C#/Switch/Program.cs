43234104a138f360610951b61ddebb27e0270401
Arthur Bernardo <arthurutschgg@gmail.com>
2021-05-11 19:41:24 -0300

    switch

---
+++ C#/Switch/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           // Declarações de switch C#
+           // Use a switchinstrução para selecionar um dos muitos blocos de código a serem executados.
+            /* 
+            switch(expressão){
+                case x:
+                bloco de codigo
+                    break;
+                case y:
+                    code block
+                    break;
+                default:
+                // code block
+                    break;
+            }
+            
+            
+            
+            */
+        
+        int day = 4;
+        switch (day) {
+            case 1:
+            Console.WriteLine("Segunda");
+            break;
+            case 2:
+            Console.WriteLine("Segunda");
+            break;
+            case 3:
+            Console.WriteLine("Segunda");
+            break;
+            case 4:
+            Console.WriteLine("Segunda");
+            break;
+            case 5:
+            Console.WriteLine("Segunda");
+            break;
+            case 6:
+            Console.WriteLine("Segunda");
+            break;
+            case 7:
+            Console.WriteLine("Segunda");
+            break;
+            case 8:
+            Console.WriteLine("Segunda");
+            break;   
+        }
+        }
+    }
+}
