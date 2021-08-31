db28935bf1f568a97188212ada57440eeb254f2e
Arthur Bernardo <arthurutschgg@gmail.com>
2021-08-31 10:36:09 -0300

    alteraçoes

--- c#/var.cs
+++
@@ -1,8 +0,0 @@
-// declaração de variáveis em C#
-
-static void Main(string[] args) { 
-    int idade, numero; // int numeros inteiros
-    double altura, peso, salario; // numeros com casas decimais/ ponto flutuante
-    string nomePai, rua, bairro, dtNasc; // palavras, frases etc.
-    bool temCasa; // true ou false, tem casa? sim ou não
-}
