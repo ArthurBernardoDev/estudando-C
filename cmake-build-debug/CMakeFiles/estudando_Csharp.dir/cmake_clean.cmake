3641881da7cb5d25e004eab18639ceaf260210b2
ArthurBernardoDev <arthurutschgg@gmail.com>
2021-10-12 16:43:35 -0300

    first commit in C

---
+++ cmake-build-debug/CMakeFiles/estudando_Csharp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/estudando_Csharp.dir/C/main.c.o"
+  "estudando_Csharp"
+  "estudando_Csharp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/estudando_Csharp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
